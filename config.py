52b41624e300a701b86230b37e94937041a35711
Zhenhui Chen <zhenhuiccc@gmail.com>
2020-04-01 16:58:14 +0800

    添加 houban oee

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# 以系统开发者人名作为key值
 projects_view_name_dict = {
     "chen_1": "智能异常事件监测与保护系统",
     "chen_2": "产品工件智能角度检测系统",
@@ -6,34 +7,39 @@ projects_view_name_dict = {
     "yv_2": "工厂CPS产线建模与分析系统",
     "wang": "工厂侧板效率智能检测系统",
     "pan": "产品包装配件完整性监测系统",
+    "yue": "厚板线OEE效率检测系统",
 }
 
 label_image_paths_dict = {
-    "chen_1": "images\\intrusion.jpg",
-    "chen_2": "images\\angle_detection.jpg",
-    "li": "images\\data_analysis.jpg",
-    "yv_1": "images\\opc_start.png",
-    "yv_2": "images\\cps_start.png",
-    "wang": "images\\xiowork.jpg",
-    "pan": "images\\robam.png",
+    "chen_1": "images/intrusion.jpg",
+    "chen_2": "images/angle_detection.jpg",
+    "li": "images/data_analysis.jpg",
+    "yv_1": "images/opc_start.png",
+    "yv_2": "images/cps_start.png",
+    "wang": "images/xiowork.jpg",
+    "pan": "images/robam.png",
+    "yue": "images/houban_oee.png",
 }
 
+# 系统启动命令 (eg. python.exe main.py)
 projects_command_dict = {
-    "chen_1": "D:\\Anaconda3\\envs\\xio\\python.exe E:\\projects-summary\\xio-intrusion-detection\\gui_main.py",
-    "chen_2": "D:\\Anaconda3\\envs\\legacy\\python.exe E:\\projects-summary\\angle-detection\\angle_start.py",
-    "li": "D:\\Anaconda3\\envs\\xio\\python.exe E:/projects-summary/QT/Main.py",
-    "yv_1": "D:\\Anaconda3\\envs\\xio\\python.exe E:/projects-summary/xio/opc_start.py",
-    "yv_2": "D:\\Anaconda3\\envs\\xio\\python.exe E:/projects-summary/xio/cps_start.py",
-    "wang": "D:\\Anaconda3\\envs\\legacy\\python.exe E:/projects-summary/xiaowork/xio_all.py",
-    "pan": "D:\\Anaconda3\\envs\\xio\\python.exe E:\\projects-summary\\component-detection-camera2\\gui_main.py",
+    "chen_1": "D:/Anaconda3/envs/xio/python.exe E:/projects-summary/xio-intrusion-detection/gui_main.py",
+    "chen_2": "D:/Anaconda3/envs/legacy/python.exe E:/projects-summary/angle-detection/angle_start.py",
+    "li": "D:/Anaconda3/envs/xio/python.exe E:/projects-summary/QT/Main.py",
+    "yv_1": "D:/Anaconda3/envs/xio/python.exe E:/projects-summary/xio/opc_start.py",
+    "yv_2": "D:/Anaconda3/envs/xio/python.exe E:/projects-summary/xio/cps_start.py",
+    "wang": "D:/Anaconda3/envs/legacy/python.exe E:/projects-summary/xiaowork/xio_all.py",
+    "pan": "D:/Anaconda3/envs/xio/python.exe E:/projects-summary/component-detection-camera2/gui_main.py",
+    "yue": "D:/Anaconda3/envs/xio/python.exe E:/projects-summary/oee_demo/gui_main.py",
 }
 
 projects_cwd_dict = {
-    "chen_1": "E:\\projects-summary\\xio-intrusion-detection",
-    "chen_2": "E:\\projects-summary\\angle-detection",
-    "li": "E:\\projects-summary",
+    "chen_1": "E:/projects-summary/xio-intrusion-detection",
+    "chen_2": "E:/projects-summary/angle-detection",
+    "li": "E:/projects-summary",
     "yv_1": "E:/projects-summary/xio",
     "yv_2": "E:/projects-summary/xio",
     "wang": "E:/projects-summary/xiaowork/",
-    "pan": "E:\\projects-summary\\component-detection-camera2",
+    "pan": "E:/projects-summary/component-detection-camera2",
+    "yue": "E:/projects-summary/oee_demo",
 }
